61031eb3541640279e22cdba0725898ae0736cb4
Saksham Dwivedi <ronakdwivedi69@gmail.com>
2025-07-08 18:18:10 +0530

    Create D23P1.c

---
+++ DSA/SakshamDwivedi_590013147/Day23/D23P1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+int size = 0;
+struct node* head = NULL;
+void createNode(int);
+void Print();
+int Traverse(int);
+int pos;
+
+int main(){
+    createNode(1);
+    createNode(2);
+    createNode(3);
+    createNode(4);
+    createNode(5);
+    Print();
+    printf("\n");
+    printf("Enter the kth position : ");
+    scanf("%d", &pos);
+    if(pos < 1 || pos > size){
+        printf("Invalid Position!");
+    }
+    else{
+        printf("%d", Traverse(pos));
+    }
+}
+
+void createNode(int data){
+       struct node* Node = (struct node*)malloc(sizeof(struct node));
+       Node -> data = data;
+       Node -> next = NULL;
+       if(head == NULL){
+            head = Node;
+            size++;
+            return;
+       }
+     struct node* current = head;
+     while(current -> next != NULL){
+      current = current -> next;
+     }
+     current -> next = Node;
+     size++; //The only reason I kept it here is for the invalid pos checking
+}
+
+void Print(){
+    struct node* current = head;
+    while(current != NULL){
+        if(current -> next != NULL){
+        printf("%d -> ", current -> data);}
+        else{printf("%d", current -> data);}
+        current = current -> next;
+    }
+
+}
+
+int Traverse(int k) {
+    struct node *fast = head;
+    struct node *slow = head;
+    for (int i = 0; i < k; i++) {
+        if (fast == NULL)
+            return -1;
+        fast = fast->next;
+    }
+    while (fast != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow->data;
+}
