2797d677c03dcb5a92d0e7b003d121e806f071b8
Ayush Mishra <ayushmishra1699417@gmail.com>
2025-07-09 22:03:47 +0530

    Update Day_24_Question_2.c

--- DSA/Ayush_Mishra_590014844/Day_24_Question_2.c
+++ DSA/Ayush_Mishra_590014844/Day_24_Question_2.c
@@ -1,12 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Insert at end
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+}
+
+// Delete N nodes after M nodes repeatedly
 struct Node* deleteNafterM(struct Node* head, int m, int n) {
     struct Node* curr = head;
     struct Node* temp;
-    int i;
     while (curr) {
-        for (i = 1; i < m && curr != NULL; i++) curr = curr->next;
-        if (curr == NULL) break;
+        // Skip M nodes
+        for (int i = 1; i < m && curr != NULL; i++)
+            curr = curr->next;
+
+        if (curr == NULL || curr->next == NULL)
+            break;
+
+        // Delete next N nodes
         temp = curr->next;
-        for (i = 0; i < n && temp != NULL; i++) {
+        for (int i = 0; i < n && temp != NULL; i++) {
             struct Node* del = temp;
             temp = temp->next;
             free(del);
@@ -17,10 +45,35 @@ struct Node* deleteNafterM(struct Node* head, int m, int n) {
     return head;
 }
 
-// Add this logic inside main after creating the list:
-int m, n;
-printf("Enter M and N: ");
-scanf("%d %d", &m, &n);
-head = deleteNafterM(head, m, n);
-printf("Modified List: ");
-printList(head);
+// Print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, val, m, d;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &val);
+        insertEnd(&head, val);
+    }
+
+    printf("Enter M (nodes to skip) and N (nodes to delete): ");
+    scanf("%d %d", &m, &d);
+
+    printf("Original List: ");
+    printList(head);
+
+    head = deleteNafterM(head, m, d);
+
+    printf("Modified List: ");
+    printList(head);
+    return 0;
+}
