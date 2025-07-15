cc4c8fe3d61ba3080a1149da561e04213f62b0a4
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-15 20:31:10 +0530

    Update and rename DAY_QUESTION3.C to Day_Question3.c

--- DSA/Krish_Mishra_590015053/Week_4/DAY24/Day_Question3.c
+++ DSA/Krish_Mishra_590015053/Week_4/DAY24/Day_Question3.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node {
     int data;
     struct Node* next;
 };
-
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
-
 void append(struct Node** headRef, int data) {
     struct Node* newNode = createNode(data);
     if (*headRef == NULL) {
@@ -24,7 +21,6 @@ void append(struct Node** headRef, int data) {
         temp = temp->next;
     temp->next = newNode;
 }
-
 struct Node* deleteAtPosition(struct Node* head, int pos) {
     if (pos <= 0 || head == NULL)
         return head;
@@ -44,14 +40,12 @@ struct Node* deleteAtPosition(struct Node* head, int pos) {
     free(temp);
     return head;
 }
-
 void printList(struct Node* head) {
     while (head != NULL) {
         printf("%d ", head->data);
         head = head->next;
     }
 }
-
 int main() {
     struct Node* head = NULL;
     int n, val, x;
