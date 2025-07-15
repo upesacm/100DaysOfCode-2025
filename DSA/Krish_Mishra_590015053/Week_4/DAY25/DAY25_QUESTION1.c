838a4f09e8438c3b67af26605c827ef6b304ad70
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-15 20:36:58 +0530

    Create DAY25_QUESTION1.c

---
+++ DSA/Krish_Mishra_590015053/Week_4/DAY25/DAY25_QUESTION1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void append(struct Node** headRef, int data) {
+    struct Node* newNode = createNode(data);
+    if (*headRef == NULL) {
+        *headRef = newNode;
+        return;
+    }
+    struct Node* temp = *headRef;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+struct Node* reverse(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* current = head;
+    struct Node* next;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+bool isPalindrome(struct Node* head) {
+    if (head == NULL || head->next == NULL)
+        return true;
+    struct Node* slow = head;
+    struct Node* fast = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    struct Node* secondHalf = reverse(slow->next);
+    struct Node* firstHalf = head;
+    struct Node* temp = secondHalf;
+    bool result = true;
+    while (temp != NULL) {
+        if (firstHalf->data != temp->data) {
+            result = false;
+            break;
+        }
+        firstHalf = firstHalf->next;
+        temp = temp->next;
+    }
+    slow->next = reverse(secondHalf);
+    return result;
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, val;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        append(&head, val);
+    }
+    if (isPalindrome(head))
+        printf("true\n");
+    else
+        printf("false\n");
+    return 0;
+}
