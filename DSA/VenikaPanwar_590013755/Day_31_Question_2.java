752296d213f17317ba11f239991335d76be4f1cb
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-16 02:34:09 +0530

    Day_31_Question_2.java

--- DSA/VenikaPanwar_590013755/Day_31_Question_2.java
+++ DSA/VenikaPanwar_590013755/Day_31_Question_2.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class MultiplyByTwo {
+public class Day_31_Question_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
