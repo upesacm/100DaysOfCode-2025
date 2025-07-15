e671ae367feb4ebad54d6227a27a2f9141a9c112
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-15 17:57:29 +0530

    Update Day_30_Q1.c

--- DSA/IshikaGarg_500124973/Day_30_Q1.c
+++ DSA/IshikaGarg_500124973/Day_30_Q1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int clearBit(int n, int i) {
+    int mask = ~(1 << i);
+
+    return n & mask;
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter the number (n): ");
+    scanf("%d", &n);
+    printf("Enter the bit position to clear (i): ");
+    scanf("%d", &i);
+
+    int result = clearBit(n, i);
+    printf("Number after clearing bit %d: %d\n", i, result);
+
+    return 0;
+}
 
