de3cfb5513e871473ba02a723cc4118798307ef9
Shreya Goyal <shreyagoyal2128@gmail.com>
2025-07-16 16:43:07 +0530

    Added Day 31 Question - Shreya Goyal 590015491

---
+++ DSA/Shreya_Goyal_590015491/Day31Question1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n > 0 && (n & (n - 1)) == 0) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
