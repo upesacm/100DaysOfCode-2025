5f308b82cc9d8f57527376e699aa0ed8dee7f6e4
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-16 22:54:07 +0530

    Update Day_31_Q3.c

--- DSA/IshikaGarg_500124973/Day_31_Q3.c
+++ DSA/IshikaGarg_500124973/Day_31_Q3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int divideByTwo(int n) {
+    return n >> 1;
+}
+
+int main() {
+    int input1 = 10;
+    int input2 = 9;
+    printf("Input 1: %d\nOutput 1: %d\n", input1, divideByTwo(input1));
+    printf("Input 2: %d\nOutput 2: %d\n", input2, divideByTwo(input2));
+    return 0;
+}
 
