81e84a5679c73d14cfbadaf38dfd711dbc8656a4
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-16 22:53:34 +0530

    Update Day_31_Q2.c

--- DSA/IshikaGarg_500124973/Day_31_Q2.c
+++ DSA/IshikaGarg_500124973/Day_31_Q2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int multiplyByTwo(int n) {
+    return n << 1;
+}
+
+int main() {
+    int input1 = 3;
+    int input2 = 7;
+    printf("Input 1: %d\nOutput 1: %d\n", input1, multiplyByTwo(input1));
+    printf("Input 2: %d\nOutput 2: %d\n", input2, multiplyByTwo(input2));
+    return 0;
+}
