5c89e6e0e8cdba9c1d3b6be0f24b21753bc1041c
Sakshi kumari <sakshi.good787@gmail.com>
2025-07-16 19:18:40 +0530

    Update Question2.py

--- DSA/Sakshi_Kumari_590012344/Day_31/Question2.py
+++ DSA/Sakshi_Kumari_590012344/Day_31/Question2.py
@@ -1,8 +1,7 @@
-def divide_by_two(n):
-    result = n >> 1
+def multiply_by_two(n):
+    result = n << 1
     return result
 number = int(input("Enter a number: "))
+answer = multiply_by_two(number)
+print("After multiplying", number, "by 2, the result is:", answer)
 
-answer = divide_by_two(number)
-
-print("After dividing", number, "by 2, the result is:", answer)
