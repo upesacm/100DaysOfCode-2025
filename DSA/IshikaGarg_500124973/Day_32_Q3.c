1e120b4696d5e743434b0b100ae6ea49191918df
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-17 21:06:25 +0530

    Update Day_32_Q3.c

--- DSA/IshikaGarg_500124973/Day_32_Q3.c
+++ DSA/IshikaGarg_500124973/Day_32_Q3.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+
+int xorArray(int arr[], int size) {
+    int xor_result = 0;
+    for (int i = 0; i < size; i++) {
+        xor_result ^= arr[i];
+    }
+    return xor_result;
+}
+
+int main() {
+    int arr1[] = {1, 2, 3};
+    int arr2[] = {4, 5, 6};
+    printf("XOR of arr1: %d\n", xorArray(arr1, 3));  // Output: 0
+    printf("XOR of arr2: %d\n", xorArray(arr2, 3));  // Output: 7
+    return 0;
+}
