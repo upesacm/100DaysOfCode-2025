1447d130782bc65f60cd6f8ab6255de08c46c0e7
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-18 22:27:48 +0530

    Update Day_33_Q3.c

--- DSA/IshikaGarg_500124973/Day_33_Q3.c
+++ DSA/IshikaGarg_500124973/Day_33_Q3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int isolateRightmostSetBit(int n) {
+    return n & -n;
+}
+
+int main() {
+    int n1 = 10;
+    int n2 = 12;
+
+    printf("Output 1: %d\n", isolateRightmostSetBit(n1));
+    printf("Output 2: %d\n", isolateRightmostSetBit(n2));
+
+    return 0;
+}
 
