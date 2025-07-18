6790b1112fced4f9be56bfad3b943458632b7c3f
Ishika0424 <ishika.garg2404@gmail.com>
2025-07-18 22:27:15 +0530

    Update Day_33_Q1.c

--- DSA/IshikaGarg_500124973/Day_33_Q1.c
+++ DSA/IshikaGarg_500124973/Day_33_Q1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool haveOppositeSigns(int a, int b) {
+    return (a ^ b) < 0;
+}
+
+int main() {
+    int a1 = 2, b1 = -3;
+    int a2 = -4, b2 = -5;
+
+    printf("Output 1: %s\n", haveOppositeSigns(a1, b1) ? "True" : "False");
+    printf("Output 2: %s\n", haveOppositeSigns(a2, b2) ? "True" : "False");
+
+    return 0;
+}
 
