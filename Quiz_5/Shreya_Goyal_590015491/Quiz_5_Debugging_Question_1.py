237784ccdbf1bcbf02d0982ce1f95da674f0456b
Shreya Goyal <shreyagoyal2128@gmail.com>
2025-07-20 20:57:30 +0530

    Added Quiz 5 Question - Shreya Goyal 590015491

---
+++ Quiz_5/Shreya_Goyal_590015491/Quiz_5_Debugging_Question_1.py
@@ -0,0 +1,18 @@
+def find_unique_pair(arr):
+    xor_sum = 0
+    for num in arr:
+        xor_sum ^= num
+
+    diff_bit = xor_sum & -xor_sum  
+
+    unique1 = 0
+    unique2 = 0
+    for num in arr:
+        if num & diff_bit: 
+            unique1 ^= num
+        else:
+            unique2 ^= num
+    return unique1, unique2
+
+#1. Bug line - if num & xor_sum:
+#2. num & xor_sum tries to split numbers using all set bits in xor_sum 
