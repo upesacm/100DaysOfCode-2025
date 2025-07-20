ca39b044e3add0c8297b91be4377499af5c3ace2
Nivedan Singh <97663156+Doller2007@users.noreply.github.com>
2025-07-20 20:22:38 +0530

    Added Quiz_5_Debugging_Question_1.py explanation

--- Quiz_5/Nivedan_590014904/Quiz_5_Debugging_Question_1.py
+++ Quiz_5/Nivedan_590014904/Quiz_5_Debugging_Question_1.py
@@ -10,7 +10,8 @@ def find_unique_pair(arr):
     unique2 = 0
     for num in arr:
         # This condition correctly groups numbers
-        if num & diff_bit:
+        """before we're using the AND operation with xor sum which doesnt do anything instead we had to use the diff_bit to XOR with"""
+        if num & diff_bit: #it was this line 
             unique1 ^= num
         else:
             unique2 ^= num
