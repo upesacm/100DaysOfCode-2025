a6385e0342bc7a9aaef77dc14065e4e1c038aad5
gautamshyamnani <gautamshyamnani@gmail.com>
2025-07-20 20:39:06 +0530

    Create Quiz_5_Debugging_Question_1.py

---
+++ Quiz_5/Gautam_Shyamnani_590013078/Quiz_5_Debugging_Question_1.py
@@ -0,0 +1,17 @@
+def find_unique_pair(arr):
+    xor_sum = 0
+    for num in arr:
+        xor_sum ^= num
+
+    
+    diff_bit = xor_sum & -xor_sum
+
+    unique1 = 0
+    unique2 = 0
+    for num in arr:
+        if num & diff_bit:
+            unique1 ^= num
+        else:
+            unique2 ^= num
+
+    return unique1, unique2
