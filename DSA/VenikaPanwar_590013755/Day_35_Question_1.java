7fdc32d01d6c1563c342c0189a4b22c13a57d4aa
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-21 20:03:07 +0530

    Day_36_Question_1.java

--- DSA/VenikaPanwar_590013755/Day_35_Question_1.java
+++ DSA/VenikaPanwar_590013755/Day_35_Question_1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Day_34_Question_1 {
+public class Day_36_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string to reverse: ");
