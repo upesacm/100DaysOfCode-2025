416d18af51dc7bb8c3c4fb369ba990296c6bd76c
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-07-24 21:39:50 +0530

    Update Day_39_Question_2.py

--- DSA/VaniAgrawal_590015846/Day_39_Question_2.py
+++ DSA/VaniAgrawal_590015846/Day_39_Question_2.py
@@ -1,27 +1,20 @@
 # Use a monotonic stack to efficiently compute stock price spans by tracking previous higher prices for optimal time-series analysis.
+prices = list(map(int, input("Enter stock prices separated by space: ").strip().split()))
+
 stack = []
-min_stack = []
+span = []
 
-def push(x):
-    stack.append(x)
-    if not min_stack or x <= min_stack[-1]:
-        min_stack.append(x)
+for i in range(len(prices)):
+   
+    while stack and prices[stack[-1]] <= prices[i]:
+        stack.pop()
+    
+    if not stack:
+        span.append(i + 1)
+    else:
+        span.append(i - stack[-1])
+    
+    stack.append(i)
 
-def pop():
-    if stack:
-        if stack[-1] == min_stack[-1]:
-            min_stack.pop()
-        return stack.pop()
-
-def get_min():
-    return min_stack[-1] if min_stack else None
-
-n = int(input("Enter number of operations: "))
-for _ in range(n):
-    cmd = input("Enter operation (push x / pop / get_min): ").split()
-    if cmd[0] == 'push':
-        push(int(cmd[1]))
-    elif cmd[0] == 'pop':
-        print("Popped:", pop())
-    elif cmd[0] == 'get_min':
-        print("Min:", get_min())
+print("Span values:")
+print(span)
