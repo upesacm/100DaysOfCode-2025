a4d0331ae31424e137fcd94a3b840f08a3a9c757
Samagra Gupta <gsamagra93@gmail.com>
2025-07-31 20:21:53 +0530

    Day 46 Question 1

--- DSA/SAMAGRA_590014891/Day_46_Question_1.c
+++ DSA/SAMAGRA_590014891/Day_46_Question_1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
 
-int findMin(int queue[], int front, int rear) {
+int findMin(int queue[], int size) {
     int min = INT_MAX;
-    for (int i = front; i <= rear; i++) {
+    for (int i = 0; i < size; i++) {
         if (queue[i] < min)
             min = queue[i];
     }
@@ -11,8 +11,15 @@ int findMin(int queue[], int front, int rear) {
 }
 
 int main() {
-    int queue[] = {4, 2, 7, 1};
-    int front = 0, rear = 3;
-    printf("%d\n", findMin(queue, front, rear));
+    int queue[100], size;
+    printf("Enter number of elements in the queue: ");
+    scanf("%d", &size);
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &queue[i]);
+    }
+
+    printf("Minimum element: %d\n", findMin(queue, size));
     return 0;
 }
