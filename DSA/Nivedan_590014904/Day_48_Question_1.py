3767e42098c78fbfeba83ad60801c0fa66bbf6ef
Nivedan Singh <97663156+Doller2007@users.noreply.github.com>
2025-08-02 22:43:48 +0530

    Create Day_48_Question_1.py

---
+++ DSA/Nivedan_590014904/Day_48_Question_1.py
@@ -0,0 +1,29 @@
+class queue:
+    def __init__(self):
+        self.data = []
+    def enqueue(self,data):
+        self.data.append(data)
+    def dequeue(self):
+        if self.data:
+            return self.data.pop(0)
+    def display(self):
+        print("Queue:", self.data)
+    def size(self):
+        return len(self.data)
+    def is_empty(self):
+        if self.data == []:
+            return True
+        else:
+            return False
+q = queue()
+n = int(input("Enter the number of elements: "))
+for i in range(n):
+    val = int(input(f"Enter the {i+1} element: "))
+    q.enqueue(val)
+x = int(input("Enter the value of x: "))
+ele = 0
+for i in range(q.size()):
+    ele = q.dequeue()
+    if ele!=x:
+        q.enqueue(ele)
+q.display()
