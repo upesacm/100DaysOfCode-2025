faaa025ee5e7e83f07fb4f01dda64290d9bc608e
Ishika Garg <ishika.garg2404@gmail.com>
2025-08-13 23:06:36 +0530

    Update Day_59_Q3.c

--- DSA/IshikaGarg_500124973/Day_59_Q3.c
+++ DSA/IshikaGarg_500124973/Day_59_Q3.c
@@ -14,8 +14,8 @@ int main() {
     int arr2[] = {10, 9, 8};
     int arr3[] = {10, 8, 9};
 
-    printf("%s\n", areHeapsIdentical(arr1, 3, arr2, 3) ? "true" : "false"); // true
-    printf("%s\n", areHeapsIdentical(arr1, 3, arr3, 3) ? "true" : "false"); // false
+    printf("%s\n", areHeapsIdentical(arr1, 3, arr2, 3) ? "true" : "false"); 
+    printf("%s\n", areHeapsIdentical(arr1, 3, arr3, 3) ? "true" : "false");
 
     return 0;
 }
