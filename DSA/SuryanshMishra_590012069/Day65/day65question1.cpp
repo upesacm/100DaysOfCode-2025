af874bb03708ae4184db0df7338abf81f7ab2446
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-19 11:34:56 +0530

    add implementation of coin change problem using greedy approach

---
+++ DSA/SuryanshMishra_590012069/Day65/day65question1.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Return minimum number of coins to make amount using greedy approach
+int minCoins(vector<int>& coins, int amount) {
+    if (amount == 0) return 0;
+    
+    // Sort coins in descending order for greedy approach
+    sort(coins.begin(), coins.end(), greater<int>());
+    
+    int coinCount = 0;
+    
+    for (int coin : coins) {
+        if (amount == 0) break;
+        
+        // Use as many coins of this denomination as possible
+        int numCoins = amount / coin;
+        coinCount += numCoins;
+        amount -= numCoins * coin;
+    }
+    
+    // If amount is not 0, it means we couldn't make exact change
+    return (amount == 0) ? coinCount : -1;
+}
+
+// Alternative version that also returns the coins used
+pair<int, vector<int>> minCoinsWithDetails(vector<int>& coins, int amount) {
+    if (amount == 0) return {0, {}};
+    
+    // Sort coins in descending order for greedy approach
+    sort(coins.begin(), coins.end(), greater<int>());
+    
+    int coinCount = 0;
+    vector<int> usedCoins;
+    
+    for (int coin : coins) {
+        if (amount == 0) break;
+        
+        // Use as many coins of this denomination as possible
+        int numCoins = amount / coin;
+        coinCount += numCoins;
+        amount -= numCoins * coin;
+        
+        // Add used coins to result
+        for (int i = 0; i < numCoins; i++) {
+            usedCoins.push_back(coin);
+        }
+    }
+    
+    // If amount is not 0, it means we couldn't make exact change
+    if (amount != 0) {
+        return {-1, {}};
+    }
+    
+    return {coinCount, usedCoins};
+}
