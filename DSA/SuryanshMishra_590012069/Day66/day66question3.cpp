88f7884dc746c3e73de2f07e38b137f309109e4d
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-20 20:45:52 +0530

    fix: remove unnecessary blank line in minCandiesWithDistribution function

--- DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
+++ DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
@@ -23,8 +23,7 @@ int minCandies(vector<int>& ratings) {
 }
 pair<int, vector<int>> minCandiesWithDistribution(vector<int>& ratings) {
     int n = ratings.size();
-    if (n == 0) return {0, {}};
-    
+    if (n == 0) return {0, {}};    
     vector<int> candies(n, 1); // Each child gets at least 1 candy
     
     // Left to right pass: ensure higher rated child gets more than left neighbor
