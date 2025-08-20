ef88d22812f4f7b77506e73d2b91abf8219e47d5
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-20 20:47:09 +0530

    fix: remove unnecessary blank line in minCandiesWithDistribution function

--- DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
+++ DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
@@ -34,8 +34,7 @@ pair<int, vector<int>> minCandiesWithDistribution(vector<int>& ratings) {
         if (ratings[i] > ratings[i + 1]) {
             candies[i] = max(candies[i], candies[i + 1] + 1);
         }
-    }
-    
+    }    
     // Calculate total candies
     int total = 0;
     for (int candy : candies) {
