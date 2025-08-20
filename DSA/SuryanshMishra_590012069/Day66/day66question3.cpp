8cc68ff6468a589bbcebb0ce3c5ecdaae3958095
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-20 20:44:10 +0530

    fix: remove unnecessary comment in minCandies function

--- DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
+++ DSA/SuryanshMishra_590012069/Day66/day66question3.cpp
@@ -5,7 +5,6 @@ int minCandies(vector<int>& ratings) {
     int n = ratings.size();
     if (n == 0) return 0;    
     vector<int> candies(n, 1);
-    // Left to right pass: ensure higher rated child gets more than left neighbor
     for (int i = 1; i < n; i++) {
         if (ratings[i] > ratings[i - 1]) {
             candies[i] = candies[i - 1] + 1;
