353c8d57bdd3dc8ea593f3295460dba6ec493091
Ishika Garg <ishika.garg2404@gmail.com>
2025-08-23 22:21:04 +0530

    Update Day_69_Q3.c

--- DSA/IshikaGarg_500124973/Day_69_Q3.c
+++ DSA/IshikaGarg_500124973/Day_69_Q3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char *s) {
+    int lastIndex[256];  // store last seen index of characters
+    for (int i = 0; i < 256; i++) lastIndex[i] = -1;
+
+    int maxLen = 0, left = 0;
+    for (int right = 0; s[right]; right++) {
+        if (lastIndex[(unsigned char)s[right]] >= left) {
+            left = lastIndex[(unsigned char)s[right]] + 1;
+        }
+        lastIndex[(unsigned char)s[right]] = right;
+        int currLen = right - left + 1;
+        if (currLen > maxLen) maxLen = currLen;
+    }
+    return maxLen;
+}
+
+int main() {
+    char s1[] = "abcabcbb";
+    char s2[] = "bbbbb";
+
+    printf("%s -> %d\n", s1, lengthOfLongestSubstring(s1));
+    printf("%s -> %d\n", s2, lengthOfLongestSubstring(s2));
+
+    return 0;
+}
 
