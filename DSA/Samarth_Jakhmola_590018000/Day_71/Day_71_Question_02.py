8c3e986c8d56d9ccca9236898e8a71e8db53c42b
Samarth-100 <samarthjakhmola544@gmail.com>
2025-08-25 18:52:44 +0530

    Create Day_71_Question_02.py

---
+++ DSA/Samarth_Jakhmola_590018000/Day_71/Day_71_Question_02.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+# Define a tree node
+class Node:
+    def __init__(self, data):
+        self.data = data    # Stores the value of the node
+        self.left = None    # Pointer / Reference to left child (None means no child)
+        self.right = None   # Pointer / Reference to right child 
+
+# Build tree from level order input
+def build_tree(values):
+    if not values or values[0] == 'N':
+        return None
+    
+    root = Node(int(values[0]))
+    queue = deque([root])    # Queue for level-order : Start with root in the queue
+    i = 1      # Index into 'values' for the next child to process
+
+    while queue and i < len(values):
+        current = queue.popleft()   # Storing the next parent whose children will be attached
+
+        # Attach left child if present 
+        if values[i] != 'N':
+            current.left = Node(int(values[i]))
+            queue.append(current.left)   # Enqueue the new child whose children will be set later
+        i += 1
+
+        if i >= len(values):
+            break
+        
+        # Attach right child if present
+        if values[i] != 'N':
+            current.right = Node(int(values[i]))
+            queue.append(current.right)   
+        i += 1                              
+
+    return root
+
+def count_leaf_nodes(root):
+    if root is None:
+        return 0
+    
+    # If node is a leaf (no left and right child)
+    if root.left is None and root.right is None:
+        return 1
+    
+    # Recursively count leaf nodes in left and right subtrees.
+    return count_leaf_nodes(root.left) + count_leaf_nodes(root.right)
+
+try:
+    values = list(input("Enter the nodes of the tree in level order ('N' where there's no node), separated by spaces : ").split())
+    print(count_leaf_nodes(build_tree(values)))
+except:
+    print("Invalid input. Please try with a valid input.")
