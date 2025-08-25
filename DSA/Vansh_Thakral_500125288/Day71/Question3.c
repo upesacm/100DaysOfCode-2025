a93923babd5bf7602f0c3f1a17b3d828ddb7dd81
Vansh Thakral <vanshnrw@gmail.com>
2025-08-25 23:23:03 +0530

    Update Question3.c

--- DSA/Vansh_Thakral_500125288/Day71/Question3.c
+++ DSA/Vansh_Thakral_500125288/Day71/Question3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+int height(struct Node* root) {
+    if (root == NULL) return 0;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    printf("Height of tree: %d\n", height(root));  
+    return 0;
+}
 
