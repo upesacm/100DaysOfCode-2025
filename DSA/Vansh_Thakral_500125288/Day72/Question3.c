6363048a5fab1f5cabcdd17b43d18239edb03bdb
Vansh Thakral <vanshnrw@gmail.com>
2025-08-26 23:11:35 +0530

    Update Question3.c

--- DSA/Vansh_Thakral_500125288/Day72/Question3.c
+++ DSA/Vansh_Thakral_500125288/Day72/Question3.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int countNodes(struct Node* root) {
+    if (root == NULL) return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+
+    printf("Total Nodes: %d\n", countNodes(root));  
+
+    return 0;
+}
 
