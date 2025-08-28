7eeb3947122de3d3ec75314cfd8ac51f152a684e
Vansh Thakral <vanshnrw@gmail.com>
2025-08-28 23:35:58 +0530

    Update Question3.c

--- DSA/Vansh_Thakral_500125288/Day73/Question3.c
+++ DSA/Vansh_Thakral_500125288/Day73/Question3.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->value = value;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+int findMax(struct Node* root) {
+    if (root == NULL) return INT_MIN;
+
+    int leftMax = findMax(root->left);
+    int rightMax = findMax(root->right);
+
+    int max = root->value;
+    if (leftMax > max) max = leftMax;
+    if (rightMax > max) max = rightMax;
+
+    return max;
+}
+
+int main() {
+
+    struct Node* root = newNode(3);
+    root->left = newNode(2);
+    root->right = newNode(5);
+
+    printf("Maximum value: %d\n", findMax(root));  
+    return 0;
+}
 
