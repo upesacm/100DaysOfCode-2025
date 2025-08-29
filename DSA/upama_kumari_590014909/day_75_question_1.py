1c768f97c5ac5dd1d6fecf1b3552f124638b12bc
upma dubey <upmakumarikaimur@gmail.com>
2025-08-29 23:48:46 +0530

    Create day_75_question_1.py

---
+++ DSA/upama_kumari_590014909/day_75_question_1.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def left_view(root):
+    if not root:
+        return []
+    
+    result = []
+    q = deque([root])
+    
+    while q:
+        level_size = len(q)
+        
+        for i in range(level_size):
+            node = q.popleft()
+            if i == 0:
+                result.append(node.data)
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+    
+    return result
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.right = Node(4)
+
+print("Left View:", left_view(root))  
