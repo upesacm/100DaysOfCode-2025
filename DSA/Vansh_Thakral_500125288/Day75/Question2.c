cc87eaed632d9ea319714aaca92e7c9f987d55c5
Vansh Thakral <vanshnrw@gmail.com>
2025-08-31 00:20:48 +0530

    Update Question2.c

--- DSA/Vansh_Thakral_500125288/Day75/Question2.c
+++ DSA/Vansh_Thakral_500125288/Day75/Question2.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+void rightViewUtil(struct Node* root, int level, int* maxLevel) {
+    if (root == NULL) return;
+
+
+    if (*maxLevel < level) {
+        printf("%d ", root->data);
+        *maxLevel = level;
+    }
+
+    
+    rightViewUtil(root->right, level + 1, maxLevel);
+    rightViewUtil(root->left, level + 1, maxLevel);
+}
+
+void rightView(struct Node* root) {
+    int maxLevel = 0;
+    rightViewUtil(root, 1, &maxLevel);
+}
+
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    printf("Right View: ");
+    rightView(root);
+    printf("\n");
+    return 0;
+}
 
