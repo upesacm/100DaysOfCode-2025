ff247d3484ee98bfa2bba9bd7ea6ba02a1631718
Vansh Thakral <vanshnrw@gmail.com>
2025-08-31 00:25:08 +0530

    Update Question2.c

--- DSA/Vansh_Thakral_500125288/Day76/Question2.c
+++ DSA/Vansh_Thakral_500125288/Day76/Question2.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+bool printAncestors(struct Node* root, int target) {
+    if (root == NULL) return false;
+
+    if (root->data == target)
+        return true;
+
+
+    if (printAncestors(root->left, target) || printAncestors(root->right, target)) {
+        printf("%d ", root->data);
+        return true;
+    }
+
+    return false;
+}
+
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    int target = 4;
+    printf("Ancestors of node %d: ", target);
+    printAncestors(root, target);
+    printf("\n");
+    return 0;
+}
 
