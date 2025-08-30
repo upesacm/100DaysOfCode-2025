f0e176be24d55c5a884cfc0bba8d8ceffd278803
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-30 13:24:12 +0530

    refactor: implement printAncestors function to display ancestor nodes in a binary tree

---
+++ DSA/SuryanshMishra_590012069/Day76/day76question2.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void printAncestors(TreeNode* root, int target) {
+        helper(root, target);
+    }
+    
+private:
+    bool helper(TreeNode* node, int target) {
+        if (!node) return false;
+        
+        // If target found, return true (but don't print current node)
+        if (node->val == target) return true;
+        
+        // Search in left and right subtrees
+        bool foundInLeft = helper(node->left, target);
+        bool foundInRight = helper(node->right, target);
+        
+        // If target found in either subtree, current node is an ancestor
+        if (foundInLeft || foundInRight) {
+            cout << node->val << " ";
+            return true;
+        }
+        
+        return false;
+    }
+};
