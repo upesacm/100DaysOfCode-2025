2638230236f3935cdd4c36adef29a5c97cfa7e27
gautamshyamnani <gautamshyamnani@gmail.com>
2025-08-31 20:40:00 +0530

    Create Quiz_11_Debugging_Question_2.py

---
+++ Quiz_11/Gautam_Shyamnani_590013078/Quiz_11_Debugging_Question_2.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def convert_to_sum_tree(root):
+    if root is None:
+        return 0
+
+    left_sum = convert_to_sum_tree(root.left)
+    right_sum = convert_to_sum_tree(root.right)
+
+    old_val = root.val
+
+    root.val = left_sum + right_sum
+
+    return old_val + root.val
+
+def print_inorder(root):
+    if root:
+        print_inorder(root.left)
+        print(root.val, end=" ")
+        print_inorder(root.right)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print("Original tree (inorder):")
+print_inorder(root)
+print()
+
+convert_to_sum_tree(root)
+
+print("After conversion to Sum Tree (inorder):")
+print_inorder(root)
+print()
