60871251f979d16ea8bd962205ad57239e9f3c47
Laksha Kundra <kundraalakshaa1@gmail.com>
2025-09-01 22:14:04 +0530

    completed question 1

---
+++ DSA/LakshaKundra_590013237/Day_78_Question_1.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def search(root, key):
+    if root is None or root.key == key:
+        return root is not None
+    if key < root.key:
+        return search(root.left, key)
+    return search(root.right, key)
+
+root = Node(5)
+root.left = Node(3)
+root.right = Node(7)
+root.left.left = Node(2)
+root.left.right = Node(4)
+root.right.left = Node(6)
+root.right.right = Node(8)
+
+print("Yes" if search(root, 4) else "No")
+print("Yes" if search(root, 10) else "No")
