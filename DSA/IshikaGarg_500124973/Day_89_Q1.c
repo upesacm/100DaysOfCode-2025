29016e685248b03a169573e2d31176bf0288098b
Ishika Garg <ishika.garg2404@gmail.com>
2025-09-13 00:04:38 +0530

    Update Day_89_Q1.c

--- DSA/IshikaGarg_500124973/Day_89_Q1.c
+++ DSA/IshikaGarg_500124973/Day_89_Q1.c
@@ -1,1 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Subset {
+    int parent, rank;
+};
+
+// Find function (with path compression)
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+}
+
+// Union function (by rank)
+void Union(struct Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+// Compare edges by weight
+int cmpEdges(const void* a, const void* b) {
+    struct Edge* e1 = (struct Edge*)a;
+    struct Edge* e2 = (struct Edge*)b;
+    return e1->weight - e2->weight;
+}
+
+int KruskalMST(int V, int E, struct Edge edges[]) {
+    qsort(edges, E, sizeof(edges[0]), cmpEdges);
+
+    struct Subset* subsets = (struct Subset*)malloc(V * sizeof(struct Subset));
+    for (int v = 0; v < V; ++v) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    int mst_weight = 0;
+    int e = 0; // count of edges in MST
+    int i = 0; // index for sorted edges
+
+    while (e < V - 1 && i < E) {
+        struct Edge next = edges[i++];
+        int x = find(subsets, next.src);
+        int y = find(subsets, next.dest);
+
+        if (x != y) {
+            mst_weight += next.weight;
+            Union(subsets, x, y);
+            e++;
+        }
+    }
+    free(subsets);
+    return mst_weight;
+}
+
+int main() {
+    int V = 4, E = 5;
+    struct Edge edges[] = {
+        {0,1,10}, {0,2,6}, {0,3,5}, {1,3,15}, {2,3,4}
+    };
+
+    printf("MST Cost (Kruskal) = %d\n", KruskalMST(V, E, edges));
+    return 0;
+}
+
 
