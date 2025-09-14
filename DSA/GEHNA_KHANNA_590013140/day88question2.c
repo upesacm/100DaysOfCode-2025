d03ca4fceae3f28daa32b86dba29592be4036a13
Gehna-01 <gehnakhanna2005@gmail.com>
2025-09-14 12:54:28 +0530

    Create day88question2.c

---
+++ DSA/GEHNA_KHANNA_590013140/day88question2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int n,m;
+int grid[100][100];
+int visited[100][100];
+
+void dfs(int x,int y){
+    if(x<0||y<0||x>=n||y>=m||grid[x][y]==0||visited[x][y]) return;
+    visited[x][y]=1;
+    dfs(x+1,y); dfs(x-1,y); dfs(x,y+1); dfs(x,y-1);
+}
+
+int main(){
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++) scanf("%d",&grid[i][j]);
+    int count=0;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            if(grid[i][j]==1&&!visited[i][j]){
+                dfs(i,j);
+                count++;
+            }
+    printf("%d\n",count);
+    return 0;
+}
