cbb942e7f6d67d6fb610e199baa1fef5a989c244
Samagra Gupta <gsamagra93@gmail.com>
2025-09-15 14:29:24 +0530

    Day 92 Question 2

---
+++ DSA/SAMAGRA_590014891/Day_92_Question_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int climbStairs(int n) {
+    if (n == 0 || n == 1) return 1;
+
+    int dp[n+1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", climbStairs(n));
+    return 0;
+}
