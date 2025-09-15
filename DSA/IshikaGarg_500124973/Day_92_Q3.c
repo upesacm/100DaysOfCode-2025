c2958c19fbc81a3b52015183ed1fd40c5c94872c
Ishika Garg <ishika.garg2404@gmail.com>
2025-09-16 01:29:25 +0530

    Update Day_92_Q3.c

--- DSA/IshikaGarg_500124973/Day_92_Q3.c
+++ DSA/IshikaGarg_500124973/Day_92_Q3.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int minCostClimbingStairs(int* cost, int n) {
+    int dp[n + 1];  // dp[i] = min cost to reach step i
+
+    dp[0] = 0;  // starting point
+    dp[1] = 0;  // can start from step 0 or 1 for free
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    return dp[n];  // min cost to reach top
+}
+
+int main() {
+    int n;
+    printf("Enter number of steps: ");
+    scanf("%d", &n);
+
+    int cost[n];
+    printf("Enter cost array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &cost[i]);
+    }
+
+    int result = minCostClimbingStairs(cost, n);
+    printf("Minimum cost to reach top = %d\n", result);
+
+    return 0;
+}
 
