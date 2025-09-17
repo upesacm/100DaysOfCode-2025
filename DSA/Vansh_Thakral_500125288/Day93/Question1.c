03fa82d4553c2cdd174d8cb177053a3012690e83
Vansh Thakral <vanshnrw@gmail.com>
2025-09-17 23:02:41 +0530

    Update Question1.c

--- DSA/Vansh_Thakral_500125288/Day93/Question1.c
+++ DSA/Vansh_Thakral_500125288/Day93/Question1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int rob(int* nums, int n) {
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+
+    int dp[n];
+    dp[0] = nums[0];
+    dp[1] = (nums[0] > nums[1]) ? nums[0] : nums[1];
+
+    for (int i = 2; i < n; i++) {
+        int rob = nums[i] + dp[i-2];   // rob current house
+        int skip = dp[i-1];            // skip current house
+        dp[i] = (rob > skip) ? rob : skip;
+    }
+    return dp[n-1];
+}
+
+int main() {
+    int houses1[] = {1,2,3,1};
+    int n1 = sizeof(houses1)/sizeof(houses1[0]);
+    printf("Max Robbery = %d\n", rob(houses1, n1)); // Output: 4
+
+    int houses2[] = {2,7,9,3,1};
+    int n2 = sizeof(houses2)/sizeof(houses2[0]);
+    printf("Max Robbery = %d\n", rob(houses2, n2)); // Output: 12
+    return 0;
+}
 
