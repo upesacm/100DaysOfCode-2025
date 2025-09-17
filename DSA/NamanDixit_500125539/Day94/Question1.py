879220a7a7a15c9e217c26a8ac32b770a7d18514
Naman Dixit <149253093+Naman-Dixit@users.noreply.github.com>
2025-09-17 23:06:32 +0530

    Create Question1.py

---
+++ DSA/NamanDixit_500125539/Day94/Question1.py
@@ -0,0 +1,10 @@
+def length_of_lis(nums):
+    n = len(nums)
+    if n == 0:
+        return 0
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
