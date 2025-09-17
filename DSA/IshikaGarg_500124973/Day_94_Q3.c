efed194c7cdaf868c8aea4f1f0f5231fa3988284
Ishika Garg <ishika.garg2404@gmail.com>
2025-09-17 23:37:46 +0530

    Update Day_94_Q3.c

--- DSA/IshikaGarg_500124973/Day_94_Q3.c
+++ DSA/IshikaGarg_500124973/Day_94_Q3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int min(int a, int b, int c) {
+    return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+}
+
+int editDistance(char *s1, char *s2) {
+    int n = strlen(s1), m = strlen(s2);
+    int dp[n+1][m+1];
+
+    for (int i = 0; i <= n; i++) dp[i][0] = i; // deletions
+    for (int j = 0; j <= m; j++) dp[0][j] = j; // insertions
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            else
+                dp[i][j] = 1 + min(dp[i-1][j],   // delete
+                                   dp[i][j-1],   // insert
+                                   dp[i-1][j-1]);// replace
+        }
+    }
+    return dp[n][m];
+}
+
+int main() {
+    char s1[] = "horse";
+    char s2[] = "ros";
+    printf("Edit Distance: %d\n", editDistance(s1, s2));
+    return 0;
+}
 
