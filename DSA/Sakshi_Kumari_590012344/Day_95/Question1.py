9bf10c68e5f3b74194a1026b1170c47a0e1cde45
Sakshi kumari <sakshi.good787@gmail.com>
2025-09-18 00:23:09 +0530

    Create Question1.py

---
+++ DSA/Sakshi_Kumari_590012344/Day_95/Question1.py
@@ -0,0 +1,6 @@
+def unique_paths(m, n):
+    dp = [[1] * n for _ in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[m - 1][n - 1]
