fc69605ee40bf2127dcfc51e68ed881b2202716a
upma dubey <upmakumarikaimur@gmail.com>
2025-09-18 23:47:07 +0530

    Create day_95_question_1.py

---
+++ DSA/upama_kumari_590014909/day_95_question_1.py
@@ -0,0 +1,9 @@
+def unique_paths(m, n):
+    dp = [[1] * n for _ in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    
+    return dp[m - 1][n - 1]
+print(unique_paths(3, 7)) 
+print(unique_paths(3, 2)) 
