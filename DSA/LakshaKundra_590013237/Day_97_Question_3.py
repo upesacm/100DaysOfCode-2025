1a34388613295ed579b462a5c7cf6b0f8fbebc95
Laksha Kundra <kundraalakshaa1@gmail.com>
2025-09-21 16:34:26 +0530

    completed question 3

---
+++ DSA/LakshaKundra_590013237/Day_97_Question_3.py
@@ -0,0 +1,14 @@
+def matrix_chain_multiplication(p):
+    n = len(p)-1
+    dp = [[0]*n for _ in range(n)]
+    for length in range(2, n+1):
+        for i in range(n-length+1):
+            j = i+length-1
+            dp[i][j] = float("inf")
+            for k in range(i, j):
+                cost = dp[i][k] + dp[k+1][j] + p[i]*p[k+1]*p[j+1]
+                dp[i][j] = min(dp[i][j], cost)
+    return dp[0][n-1]
+
+print(matrix_chain_multiplication([1,2,3,4]))
+print(matrix_chain_multiplication([10,20,30]))
