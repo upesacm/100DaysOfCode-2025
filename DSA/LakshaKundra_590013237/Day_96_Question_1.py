5c03a5fb1f33ad1010b86bfb82774bac5a665127
Laksha Kundra <kundraalakshaa1@gmail.com>
2025-09-21 16:27:24 +0530

    completed question 1

---
+++ DSA/LakshaKundra_590013237/Day_96_Question_1.py
@@ -0,0 +1,13 @@
+def knapsack(weights, values, W):
+    n = len(weights)
+    dp = [[0]*(W+1) for _ in range(n+1)]
+    for i in range(1, n+1):
+        for w in range(1, W+1):
+            if weights[i-1] <= w:
+                dp[i][w] = max(values[i-1] + dp[i-1][w-weights[i-1]], dp[i-1][w])
+            else:
+                dp[i][w] = dp[i-1][w]
+    return dp[n][W]
+
+print(knapsack([1,3,4,5],[1,4,5,7],7))
+print(knapsack([4,5,1],[1,2,3],4))
